a19b96ef54c11fda8511476481aced81d41f4396
GALEN\zg919 <919506719@qq.com>
2024-01-21 14:40:31 +0800

    嵌入fragment

--- MixFlutter/app/src/main/java/com/example/mixflutter/MainActivity.kt
+++ MixFlutter/app/src/main/java/com/example/mixflutter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mixflutter.databinding.ActivityMainBinding
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragment
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -17,8 +19,18 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        //todo 使用预热engine 更快渲染
+        //https://flutter.cn/docs/add-to-app/android/add-flutter-fragment?tab=launch-with-custom-entrypoint-kotlin-tab
+        val flutterFragment = FlutterFragment.createDefault()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer,flutterFragment).commit()
         binding.tvTest.setOnClickListener {
-            startActivity(Intent(this,FlutterActivity::class.java))
+            val intent = FlutterActivity
+                .withNewEngine()
+//                .initialRoute("/my_route")
+                .build(this)
+            startActivity(intent)
+//            startActivity(Intent(this,FlutterActivity::class.java))
         }
     }
 }
